e154fb163b80383a50ada8be6a79738bd6eb2172
Alexey Trukhlyayev <exact84@gmail.com>
2025-03-27 08:38:23 +0500

    feat: 9 tasks done

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -141,8 +141,17 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    for (let i = 0; i <= attempts; i += 1) {
+      try {
+        return func();
+      } catch (e) {
+        if (i === attempts) throw e;
+      }
+    }
+    return undefined;
+  };
 }
 
 /**
@@ -168,8 +177,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsStr = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${argsStr}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argsStr}) ends`);
+    return result;
+  };
 }
 
 /**
